ea4600991322ea654fe7b9e717cd7d97da1161b4
ah-hyeon <sepo_o@daum.net>
2018-04-20 06:18:27 +0000

    pecifying and unsecuring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'wall/write'
+  get 'wall/write'  => 'wall#write'
 
-  get 'wall/posts'
+  get 'wall/posts' => 'wall#posts'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
