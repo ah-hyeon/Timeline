d1d145d320206c58b0c2346e31f11c9840461f73
ah-hyeon <sepo_o@daum.net>
2018-04-22 12:06:36 +0000

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
-Rails.application.routes.draw do
+Timeline::Application.routes.draw do
   get 'wall/write'  => 'wall#write'
 
-  get 'wall/posts' => 'wall#posts'
+  post 'wall/posts' => 'wall#posts'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
